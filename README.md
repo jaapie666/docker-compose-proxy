8159ffbf7f849e894bda9017967098224b24ae48
jaapie <jaapie@timegoat.org>
2018-10-15 16:28:03 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+readme for proxy stack
+
+Replace environment variables in "proxy-environment-variables.sh" and then run 
+  $ source proxy-environment-variables && docker-compose up -d
---
+++ proxy-environment-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#  Environment variables for proxy autoconfiguration
+
+#  Domain name to be used for reverse proxy
+export LE_DOMAINNAME="domain.tld"
+
+#  Email address for Let's Encrypt
+export LE_EMAIL="example@email.com"
+
+#  Subdomain to be used for portainer service
+
+export PORTAINER_HOSTNAME="hostname"
